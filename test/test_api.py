2b8588ef35c4eb6159a8c7d077f52a5b803fc246
Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2014-01-27 19:08:47 +0100

    Add test_api.py.

---
+++ test/test_api.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+# Copyright 2014 Jan-Philip Gehrcke. See LICENSE file for details.
+
+import os
+import sys
+import datetime
+import tempfile
+
+sys.path.insert(0, os.path.abspath('..'))
+from deletebytime import Filter, FileSystemEntry
+
+WINDOWS = sys.platform == "win32"
+
+# py.test runs tests by order of definition. This is useful for running simple,
+# fundamental tests first and more complex tests later.
+from py.test import raises, mark
+
+import logging
+logging.basicConfig(
+    format='%(asctime)s,%(msecs)-6.1f [%(process)-5d]%(funcName)s# %(message)s',
+    datefmt='%H:%M:%S')
+log = logging.getLogger()
+log.setLevel(logging.DEBUG)
+
+#LONG = 999999
+#SHORTTIME = 0.01
+#ALMOSTZERO = 0.00001
+#LONGERTHANBUFFER = "A" * 9999999
+
+
+class TestBasicFSEntry(object):
+    """Test basic FileSystemEntry logic.
+
+    Upon creation, an FSE extracts information about the given path with a
+    stat() system call via Python's `stat` module. From this data, the FSE
+    populates itself with various convenient attributes.
+
+    Flow for each test_method:
+        o = TestClass()
+        o.setup()
+        try:
+            o.test_method()
+        finally:
+            o.teardown()
+    """
+    def setup(self):
+        pass
+
+    def teardown(self):
+        pass
+
+    def test_dir(self):
+        fse = FileSystemEntry(path='.')
+        assert fse.type == 'dir'
+        assert isinstance(fse.modtime, datetime)
+
+    def test_file(self):
+        with tempfile.NamedTemporaryFile() as t:
+            fse = FileSystemEntry(path=t.name)
+            assert fse.type == 'file'
+            assert isinstance(f.modtime, datetime)
+
+    def test_symlink(self):
+        # set up symlink
+        fse.type == 'symlink'
+        assert isinstance(f.modtime, datetime)
+
+
+class TestBasicFilter(object):
+    """Test basic Filter logic.
+    """
+    def setup(self):
+        pass
+
+    def teardown(self):
+        pass
+
+    def test_singlemsg_short_bin(self):
+        fse = FileSystemEntry(
+            path='',
+            )
+        f = Filter(....)
+        reject = f.filter([fse])
+        ...
