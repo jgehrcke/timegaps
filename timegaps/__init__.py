4d4acbaef517097eb3fbb923d52978bb96338853
Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2014-02-06 19:13:50 +0100

    Add header and version to __init__.

--- timegaps/__init__.py
+++ timegaps/__init__.py
@@ -1,2 +1,6 @@
+# -*- coding: utf-8 -*-
+# Copyright 2014 Jan-Philip Gehrcke. See LICENSE file for details.
+
+__version__ = '0.1.0'
 from timegaps import FileSystemEntry, TimegapsError
 from timefilter import TimeFilter, TimeFilterError
