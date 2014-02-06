a21da0c96d6f6bfd2d7b1e364e907aa440a9ccd8
Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2014-02-06 14:58:44 +0100

    Add audit.sh.

---
+++ audit.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo ">>> Running timegaps unit tests, investigate code coverage..."
+cd test && py.test --cov timegaps
+if [ $? -eq 0 ]; then
+    if [ -d coverage_html ]; then
+        rm -rf coverage_html/*
+    fi
+    coverage html -d coverage_html
+fi
+exit
+cd ..
+echo -e "\n>>> Running setup.py check..."
+python setup.py check
+echo -e "\n>>> Running python setup.py --long-description | rst2html.py > /dev/null..."
+python setup.py --long-description | rst2html.py > /dev/null
+echo -e "\n>>> Running rst2html.py CHANGELOG.rst > /dev/null..."
+rst2html.py CHANGELOG.rst > /dev/null
+echo -e "\n>>> Running PEP8 check..."
+pep8
+echo -e "\n>>> Running pylint..."
+pylint --reports=n --disable=C0103,W0212,W0511,W0142,R0903 gipc/gipc.py
