a79db6b110f7c2859621478ec92b3df05f029358
Robert Marklund <robbelibobban@gmail.com>
2024-01-22 18:26:37 +0100

    add time-regex switch for easier parsing of time in strings/filenames
    Signed-off-by: Robert Marklund <robbelibobban@gmail.com>

--- timegaps-runner.py
+++ timegaps-runner.py
@@ -7,7 +7,7 @@ Convenience wrapper for testing development version of timegaps CLI without
 installing. With the root directory of this repository as CWD, timegaps can be
 invoked via
 
-$ python timegaps-runner.py 
+$ python timegaps-runner.py
 
 
 The canonical way would be (http://stackoverflow.com/a/3617928/145400):
